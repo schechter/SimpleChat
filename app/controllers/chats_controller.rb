420c9abfa1e2534f0a80941084f0a1b076df9216
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-11 15:04:18 -0400

    Sending message events out on Pusher

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,7 +6,13 @@ class ChatsController < ApplicationController
   def send_chat
     puts 'Hello!'
     puts params
-
+    puts params[:chat_input]
+    puts Pusher.app_id
+    puts Pusher.key
+    puts Pusher.secret
+    #Pusher.trigger(channel_name, event_name, data_to_send_with_event)
+    Pusher.trigger('chatroom', 'message', params[:chat_input])
+    render nothing: true
   end
 
 end
