003cec610af397da9a051e494d1d6b60dc9518c8
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-11 14:33:06 -0400

    Setting up route for messaging

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,4 +3,10 @@ class ChatsController < ApplicationController
   def index
   end
 
+  def send_chat
+    puts 'Hello!'
+    puts params
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 SimpleChat::Application.routes.draw do
   root to: 'chats#index'
+
+  post '/chats/send_chat', to: 'chats#send_chat', as: 'send_chat'
+
 end
