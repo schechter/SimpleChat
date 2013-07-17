da30985c111015b15dc7f3c29b6b30348de9e084
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-17 12:04:49 -0400

    Removing extraneous debugging and adding comment explaining the move from Pusher initialization in JS to HTML

--- config/initializers/pusher_init.rb
+++
@@ -1,7 +0,0 @@
-# if Rails.env.development?
-#   Pusher.app_id = ENV['PUSHER_APP']
-#   Pusher.key = ENV['PUSHER_KEY']
-#   Pusher.secret = ENV['PUSHER_SECRET']
-# else if Rails.env.production?
-#   Pusher
-# end
